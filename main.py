bcf76eb0065e412f0d5366e5a95b62105a6482f2
lefty <geza.balogh@trayport.com>
2026-02-08 20:22:48 +0100

    first version that passes tcs 1,2,5,7,11, but fails the rest

---
+++ main.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'findSmallestMissingPositive' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY orderNumbers as parameter.
+#
+
+def findSmallestMissingPositive(orderNumbers):
+    # Write your code here
+    #print(orderNumbers)
+    if len(orderNumbers) == 0:
+        return 1
+    sortedOrderNumbers = sorted(orderNumbers)
+    base = sortedOrderNumbers[0]
+    for index in range(1, len(sortedOrderNumbers)):
+
+        #print(f"index {index};  sortedOrderNumbers: {sortedOrderNumbers[index]}")
+        if sortedOrderNumbers[index] >= 0 and base+index < sortedOrderNumbers[index]:
+            return base + 1
+        base = sortedOrderNumbers[index]
+    return max(sortedOrderNumbers) + 1
+
+
+
+if __name__ == '__main__':
+    orderNumbers_count = int(input().strip())
+
+    orderNumbers = []
+
+    for _ in range(orderNumbers_count):
+        orderNumbers_item = int(input().strip())
+        orderNumbers.append(orderNumbers_item)
+
+    result = findSmallestMissingPositive(orderNumbers)
+
+    print(result)
